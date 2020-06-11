0527f588f2ee40f4aecfbdc35eb25e5e963812f7
Pedro Belfort <belfortdev@gmail.com>
2020-06-11 14:01:14 -0300

    Set up project

---
+++ LutickXP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LutickXP
+//
+//  Created by Pedro Belfort on 6/11/20.
+//  Copyright © 2020 Lutick. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Lutick!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
